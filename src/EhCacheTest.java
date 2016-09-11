e210990053085108f3f1a2d5a25aa14f3b1d6da9
afzhxlisi <afzhxlisi@126.com>
2016-09-11 23:48:57 +0800

    add cache

---
+++ src/EhCacheTest.java
@@ -0,0 +1,25 @@
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.service.UserService;
+
+@RunWith(SpringJUnit4ClassRunner.class)		//表示继承了SpringJUnit4ClassRunner类
+@ContextConfiguration(locations = {"classpath:application.xml"})
+
+public class EhCacheTest {
+
+	public static Logger logger = LoggerFactory.getLogger(EhCacheTest.class);
+	@Resource
+	UserService userService;
+	
+	@Test
+	public void testAll(){
+		logger.info(userService.find(null).toString());
+	}
+}
---
+++ src/com/service/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.stereotype.Service;
+
+import com.domain.User;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	public static Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
+	
+	@CachePut(value="users",key="123")
+	@Override
+	   public User find(User user) {
+			logger.info("db");
+			return new User();
+
+	   }
+
+}
