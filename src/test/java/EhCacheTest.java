2dd6d1527e2da6f21c7eb962afc2a0d4aec9fba3
afzhxlisi <afzhxlisi@126.com>
2016-09-12 22:22:26 +0800

    success sync

--- src/test/java/EhCacheTest.java
+++ src/test/java/EhCacheTest.java
@@ -4,12 +4,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.service.UserService;
 
-@RunWith(SpringJUnit4ClassRunner.class)		//±íÊ¾¼Ì³ÐÁËSpringJUnit4ClassRunnerÀà
+@RunWith(SpringJUnit4ClassRunner.class)		//ï¿½ï¿½Ê¾ï¿½Ì³ï¿½ï¿½ï¿½SpringJUnit4ClassRunnerï¿½ï¿½
 @ContextConfiguration(locations = {"classpath:application.xml"})
 
 public class EhCacheTest {
@@ -20,6 +23,53 @@ public class EhCacheTest {
 	
 	@Test
 	public void testAll(){
-		logger.info(userService.find(null).toString());
+		try {
+//			logger.info(userService.find(null).toString());
+			Thread.sleep(10000);
+			logger.info(userService.find(null).toString());
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public  static void main(String[] args){
+		ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+//		context.getBean(UserService.class).find(null);
+//		context.getBean(CacheManager.class).getCache("users").put("123", "123");
+		logger.info(context.getBean(CacheManager.class).getCache("users").get("123").get().toString());
+		logger.info(context.getBean(CacheManager.class).getCache("users").get("234").get().toString());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		logger.info(context.getBean(CacheManager.class).getCache("users").get("123").get().toString());
+		boolean flag;
+		flag =(context.getBean(CacheManager.class).getCache("users").get("234")!=null);
+		logger.info(String.valueOf(flag));
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		logger.info(context.getBean(CacheManager.class).getCache("users").get("123").get().toString());
+		logger.info(context.getBean(CacheManager.class).getCache("users").get("234").get().toString());
+		try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
